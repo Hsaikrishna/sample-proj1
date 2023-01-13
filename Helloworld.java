5032e938816d3d4692b94244010fe3c4ce09f99f
Hsaikrishna <120639890+Hsaikrishna@users.noreply.github.com>
2023-01-13 22:50:43 +0530

    commit changed file

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,5 @@
-Public class Helloworld{
-
-public static void main(String []args){
-System.out.println("Hello wolrd");
-Systen.out.print("hai");
+public class Helloworld{
+ public static void main(String []args){
+ System.out.println("Hello World@@");
 }
 }
